6e0e485014fd0c34d5d1f1f190dd601c1db06aa1
Emmanuel <emmanuelpontsho553@gmail.com>
2024-06-29 06:18:20 +0200

    Django datase activation

--- MyStore/settings.py
+++ MyStore/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'MyStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PASSWORD': my_db_pass,
-        'PORT': '42156',     
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'railway',
+        #'USER': 'postgres',
+        #'HOST': 'roundhouse.proxy.rlwy.net',
+        #'PASSWORD': my_db_pass,
+        #'PORT': '42156',     
     }
 }
 
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
